18777f4f67449f2173d940718c9b02e0be4a9327
k1LoW <k1lowxb@gmail.com>
2019-08-04 16:16:40 +0900

    Add `metr list`

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -5,45 +5,57 @@ import (
 	"time"
 )
 
+type Metric struct {
+	Name        string
+	Description string
+	Format      string
+}
+
 // Metrics struct
 type Metrics struct {
 	sync.Map
-	keys []string
+	metrics []Metric
 }
 
 // NewMetrics returns *Metrics
 func NewMetrics() *Metrics {
 	m := &Metrics{
-		keys: []string{
-			"cpu",
-			"mem",
-			"swap",
-			"user",
-			"system",
-			"idle",
-			"nice",
-			"iowait",
-			"irq",
-			"softirq",
-			"steal",
-			"guest",
-			"guest_nice",
-			"stolen",
-			"load1",
-			"load5",
-			"load15",
+		metrics: []Metric{
+			{"cpu", "Percentage of cpu used.", "%f"},
+			{"mem", "Percentage of RAM used.", "%f"},
+			{"swap", "Amount of memory that has been swapped out to disk (bytes).", "%d"},
+
+			{"user", "Percentage of CPU utilization that occurred while executing at the user level.", "%f"},
+			{"system", "Percentage of CPU utilization that occurred while executing at the system level.", "%f"},
+			{"idle", "Percentage of time that CPUs were idle and the system did not have an outstanding disk I/O request.", "%f"},
+			{"nice", "Percentage of CPU utilization that occurred while executing at the user level with nice priority.", "%f"},
+			{"iowait", "Percentage of time that CPUs were idle during which the system had an outstanding disk I/O request.", "%f"},
+			{"irq", "Percentage of time spent by CPUs to service hardware interrupts.", "%f"},
+			{"softirq", "Percentage of time spent by CPUs to service software interrupts.", "%f"},
+			{"steal", "Percentage of time spent in involuntary wait by the virtual CPUs while the hypervisor was servicing another virtual processor.", "%f"},
+			{"guest", "Percentage of time spent by CPUs to run a virtual processor.", "%f"},
+			{"guest_nice", "Percentage of time spent by CPUs to run a virtual processor with nice priority.", "%f"},
+
+			{"load1", "Load avarage for 1 minute.", "%f"},
+			{"load5", "Load avarage for 5 minutes.", "%f"},
+			{"load15", "Load avarage for 15 minutes.", "%f"},
 		},
 	}
 	return m
 }
 
+// List returns metric info list
+func (m *Metrics) List() []Metric {
+	return m.metrics
+}
+
 // Each returns ordered metrics
 func (m *Metrics) Each(f func(key string, value interface{})) {
-	for _, key := range m.keys {
-		if value, ok := m.Load(key); ok {
-			f(key, value)
+	for _, metric := range m.metrics {
+		if value, ok := m.Load(metric.Name); ok {
+			f(metric.Name, value)
 		} else {
-			f(key, 0)
+			f(metric.Name, 0)
 		}
 	}
 }
